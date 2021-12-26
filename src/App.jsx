cc64465c095a75ecf96319fcbef21f15fa8a3713
kato9292929 <76525458+kato9292929@users.noreply.github.com>
2021-12-26 23:34:25 +0900

    4 4

--- src/App.jsx
+++ src/App.jsx
@@ -55,19 +55,6 @@ export const App = () => {
         onClickDelete={onClickDelete}
       />
       <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
-      <div className="complete-area">
-        <p className="title">完了のTODO</p>
-        <ul>
-          {completeTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickBack(index)}>戻す</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
     </>
   );
 };
